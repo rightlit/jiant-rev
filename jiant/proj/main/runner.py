aeb39945a1d036e192f6e8bf09a2fb134a337db2
rightlit <rightlit@gmail.com>
2021-09-15 05:38:39 +0000

    src update

--- jiant/proj/main/runner.py
+++ jiant/proj/main/runner.py
@@ -111,7 +111,8 @@ class JiantRunner:
         for i in range(task_specific_config.gradient_accumulation_steps):
             print('##### step : ', i)
             batch, batch_metadata = train_dataloader_dict[task_name].pop()
-            print('##### batch : ', batch)
+            print('##### batch.len() : ', len(batch))
+            print('##### batch.keys() : ', batch.keys())
             batch = batch.to(self.device)
             model_output = wrap_jiant_forward(
                 jiant_model=self.jiant_model, batch=batch, task=task, compute_loss=True,
