e5e5827e2085943f72152262ece0525192395d97
rightlit <rightlit@gmail.com>
2021-09-27 00:42:31 +0000

    src update

--- jiant/scripts/download_data/utils.py
+++ jiant/scripts/download_data/utils.py
@@ -25,6 +25,8 @@ def convert_hf_dataset_to_examples(
     Returns:
         Dict[phase] -> list[examples]
     """
+    # for cola
+    # column_names =['source',	'acceptability_label',	'source_annotation',	'sentence'])
 
     print('##### load_dataset(), path=', path, ', name=', name)
     #dataset = datasets.load_dataset(path=path, name=name, version=version)
@@ -32,7 +34,7 @@ def convert_hf_dataset_to_examples(
         dataset = datasets.load_dataset('csv', 
             data_files={'train': '/content/NIKL_CoLA_train.tsv', 'validation': '/content/NIKL_CoLA_dev.tsv', 'test': '/content/NIKL_CoLA_test.tsv'}, 
             delimiter='\t',
-            column_names =['source',	'acceptability_label',	'source_annotation',	'sentence'])
+            column_names =['source',	'label',	'source_annotation',	'sentence'])
 
     if phase_map:
         for old_phase_name, new_phase_name in phase_map.items():
