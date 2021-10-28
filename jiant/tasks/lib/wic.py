a7d35b8f4e3a4064007156f9395e236d57ac193f
jeongwook lee <rightlit@gmail.com>
2021-10-28 11:43:30 +0900

    Update wic.py

--- jiant/tasks/lib/wic.py
+++ jiant/tasks/lib/wic.py
@@ -90,7 +90,8 @@ class Example(BaseExample):
                 span_start_char =  char_span.start
             # added code (for exception)
             if(exception_flag and span_text_char == 1):
-                span_text_char = span_text_char + 1
+                #span_text_char = span_text_char + 1
+                span_start_char =  span_start_char - 1
 
             aligner = retokenize.TokenAligner(
                 sentence_normed_space_tokenization, sentence_normed_target_tokenization
